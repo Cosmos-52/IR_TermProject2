6d0b0c10bb8bdcab148c959f34e7fa8e659f0c0f
Cosmos-52 <hongjin_zhu@cmu.ac.th>
2022-03-14 01:41:34 +0800

    remove the asset cause it too big

--- fileParse.py
+++ fileParse.py
@@ -2,20 +2,20 @@ import numpy as np
 import pandas as pd
 import string
 from textblob import TextBlob
+
 import nltk
-
-import fileParse
-
 nltk.download('stopwords')
 nltk.download('punkt')
 from nltk.corpus import stopwords
 from nltk.stem import PorterStemmer
+
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
+from sklearn.feature_extraction.text import CountVectorizer
+tfidf = CountVectorizer()
 
-data = pd.read_csv(
-    '/Users/zhuhongjin/大三下/953481_IR/assignment/TermProject2/asset/archive/Food Ingredients and Recipe Dataset with Image Name Mapping.csv')
-# title = data.pop('Title')
+
+data = pd.read_csv('asset/archive/recipe.csv')
 
 # remove the row that the content is broken
 tmp = data['Title']
@@ -25,28 +25,36 @@ for i, v in nan.items():
         data = data.drop(index=i)
         # print(i)
 
-
+# check whether the broken content is still exist
 # fix = data['Title']
 # test = fix.isnull()
 # for i,v in test.items():
 #     if(v):
 #         print(i)
+# clean_title = data['Title']
+# clean_title = clean_title.apply(lambda s: s.translate(str.maketrans('', '', string.punctuation + u'\xa0')))
+# clean_title = clean_title.apply(lambda s: s.lower()).apply(lambda s: s.translate(str.maketrans(string.whitespace, ' ' * len(string.whitespace), ''))).drop_duplicates()
+# clean_title = clean_title.apply(lambda s: [x.strip() for x in s.split()])
 
-# dropedData = data.drop(columns=data.columns[:1], axis=1).drop(['Cleaned_Ingredients'], axis=1).drop(['Instructions'], axis=1)
-# title = dropedData['Title'].squeeze()
+
+# q = 'salt'
+# tfidfvectorizer = TfidfVectorizer(ngram_range=(1,2))
+# title_vec = tfidfvectorizer.fit_transform(clean_title)
+# query = tfidfvectorizer.transform(q)
+# result = cosine_similarity(title_vec, query).reshape((-1,))
+
+
 def check_spell(query, query_type):
-    if query is '':
-        return print('please fill something')
     check = TextBlob(query).correct()
     correct = str(check)
     if query_type == 'title':
-        search_by_title([correct])
+        query_by_title([correct])
     else:
         search_by_ingredients([correct])
 
 
-
 def get_cleandata_and_unique(data):
+    clean_title = data['Title']
     clean_data = data.apply(lambda s: s.translate(str.maketrans('', '', string.punctuation + u'\xa0')))
     clean_data = clean_data \
         .apply(lambda s: s.lower()) \
@@ -54,25 +62,44 @@ def get_cleandata_and_unique(data):
         .drop_duplicates()
     clean_data = clean_data.apply(lambda s: [x.strip() for x in s.split()])
 
-    sw_set = set(stopwords.words())
-    no_sw_description = clean_data.apply(lambda x: [w for w in x if w not in sw_set])
-    ps = PorterStemmer()
-    stemmed_description = no_sw_description.apply(lambda x: set([ps.stem(w) for w in x]))
-    all_unique_term = list(set.union(*stemmed_description.to_list()))
-    invert_idx = {}
-    for s in all_unique_term:
-        invert_idx[s] = set(stemmed_description.loc[stemmed_description.apply(lambda x: s in x)].index)
-    return clean_data, invert_idx
+    # sw_set = set(stopwords.words())
+    # no_sw_description = clean_data.apply(lambda x: [w for w in x if w not in sw_set])
+    # ps = PorterStemmer()
+    # stemmed_description = no_sw_description.apply(lambda x: set([ps.stem(w) for w in x]))
+    # all_unique_term = list(set.union(*stemmed_description.to_list()))
+    # invert_idx = {}
+    # for s in all_unique_term:
+    #     invert_idx[s] = set(stemmed_description.loc[stemmed_description.apply(lambda x: s in x)].index)
+    return clean_data
+    # return clean_data, invert_idx
 
 
-def search_by_title(query):
-    invert_idx = fileParse.invert_title
-    to_search_title = query  # query should be list
-    ps = PorterStemmer()
-    stemmed_title = np.unique([ps.stem(w) for w in to_search_title])
-    searched_title = sorted(set.union(*[invert_idx[s] for s in stemmed_title]))
-    print(query, searched_title)
-    return searched_title
+def query_by_title():
+    q = 'salt'
+    tfidfvectorizer = TfidfVectorizer(ngram_range=(1,2))
+    title_vec = tfidfvectorizer.fit_transform(clean_data)
+    query = tfidfvectorizer.transform(q)
+    result = cosine_similarity(title_vec, query).reshape((-1,))
+    for i, index in enumerate(result.argsort()[-10:][::-1]):
+        print(str(i + 1), fileParse.data['Title'][index], "--", result[index])
+# def search_by_title(query):
+#     invert_idx = fileParse.invert_title
+#     query_vec = tfidf.transform([query])  # query should be list
+#     to_search_title= cosine_similarity(X,query_vec).reshap((-1))
+#     ps = PorterStemmer()
+#     stemmed_title = np.unique([ps.stem(w) for w in to_search_title])
+#     searched_title = sorted(set.union(*[invert_idx[s] for s in stemmed_title]))
+#     print(query, searched_title)
+#     return searched_title
+
+# def search_by_title(query):
+#     invert_idx = fileParse.invert_title
+#     to_search_title = query  # query should be list
+#     ps = PorterStemmer()
+#     stemmed_title = np.unique([ps.stem(w) for w in to_search_title])
+#     searched_title = sorted(set.union(*[invert_idx[s] for s in stemmed_title]))
+#     print(query, searched_title)
+#     return searched_title
 
 
 def search_by_ingredients(query):
@@ -87,37 +114,14 @@ def search_by_ingredients(query):
 
 title = data['Title']
 tmpTitle = get_cleandata_and_unique(title)
-clean_title, invert_title = tmpTitle
+clean_title = tmpTitle
+# X = tfidf.fit_transform(clean_title).tocoo()
 
 ingredients = data['Ingredients']
 temIngredients = get_cleandata_and_unique(ingredients)
-clean_ingredients, invert_ingredients = temIngredients
+clean_ingredients = temIngredients
+
 
 if __name__ == '__main__':
-    check_spell('mushro', 'title')
-    check_spell('sugar', 'ingredients')
-
-# -------------------
-# clean_title = title.apply(lambda s: s.translate(str.maketrans('', '', string.punctuation + u'\xa0')))
-# clean_title = clean_title \
-#     .apply(lambda s: s.lower()) \
-#     .apply(lambda s: s.translate(str.maketrans(string.whitespace, ' ' * len(string.whitespace), ''))) \
-#     .drop_duplicates()
-# clean_title = clean_title.apply(lambda s: [x.strip() for x in s.split()])
-#
-# sw_set = set(stopwords.words())
-# no_sw_description = clean_title.apply(lambda x: [w for w in x if w not in sw_set])
-# ps = PorterStemmer()
-# stemmed_description = no_sw_description.apply(lambda x: set([ps.stem(w) for w in x]))
-# all_unique_term = list(set.union(*stemmed_description.to_list()))
-#
-# invert_idx = {}
-# for s in all_unique_term:
-#     invert_idx[s] = set(stemmed_description.loc[stemmed_description.apply(lambda x: s in x)].index)
-#
-# to_search_title = ['Peanut']
-# stemmed_title = np.unique([ps.stem(w) for w in to_search_title])
-# searched_title = sorted(set.union(*[invert_idx[s] for s in stemmed_title]))
-#
-# print(clean_title.loc[searched_title].apply(lambda x: ' '.join(x)).head().to_markdown())
-# ---------------------------
+    # check_spell('mushro', 'title')
+    # check_spell('sugar', 'ingredients')
---
+++ test.py
@@ -0,0 +1,96 @@
+import pandas as pd
+
+from model.bm25 import BM25
+from service.spell_checker_service import spell_checking
+
+food = pd.read_csv('./data/food.csv')
+food.drop('Unnamed: 0', axis='columns',inplace=True)
+food.drop('Ingredients', axis='columns',inplace=True)
+
+bm25 = BM25()
+bm25.fit(food["Title"].astype('U'))
+
+
+def search_by_bm25(query):
+    result = bm25.transform(query, food["Title"].astype('U'))
+    return result.argsort()[-50:][::-1]
+
+
+def search_by_bm25_ingredient(ingredient):
+    result = bm25.transform(ingredient, food["Cleaned_Ingredients"].astype('U'))
+    return result.argsort()[-50:][::-1]
+
+
+def search_by_bm25_instruction(instr):
+    result = bm25.transform(instr, food["Instructions"].astype('U'))
+    return result.argsort()[-50:][::-1]
+
+
+def search_by_title(query):
+    return_data = []
+    result = search_by_bm25(query)
+    for i in result:
+        data = {
+            'id': int(i),
+            'title': food.iloc[i].to_dict()["Title"],
+            'instructions': [x.lstrip().strip("'") for x in str(food.iloc[i].to_dict()["Instructions"]).split('.')],
+            'image_Name': food.iloc[i].to_dict()["Image_Name"],
+            'ingredients': [x.lstrip().strip("'") for x in food.iloc[i].to_dict()["Cleaned_Ingredients"].strip("][").split(',')],
+        }
+        return_data.append(data)
+    return return_data
+
+
+def search_by_ingreidents(query):
+    return_data = []
+    result = search_by_bm25_ingredient(query)
+    for i in result:
+        data = {
+            'id': int(i),
+            'title': food.iloc[i].to_dict()["Title"],
+            'instructions': [x.lstrip().strip("'") for x in str(food.iloc[i].to_dict()["Instructions"]).split('.')],
+            'image_Name': food.iloc[i].to_dict()["Image_Name"],
+            'ingredients': [x.lstrip().strip("'") for x in food.iloc[i].to_dict()["Cleaned_Ingredients"].strip("][").split(',')],
+        }
+        return_data.append(data)
+    return return_data
+
+
+def search_by_instruciton(query):
+    return_data = []
+    result = search_by_bm25_instruction(query)
+    for i in result:
+        data = {
+            'id': int(i),
+            'title': food.iloc[i].to_dict()["Title"],
+            'instructions': [x.lstrip().strip("'") for x in str(food.iloc[i].to_dict()["Instructions"]).split('.')],
+            'image_Name': food.iloc[i].to_dict()["Image_Name"],
+            'ingredients': [x.lstrip().strip("'") for x in food.iloc[i].to_dict()["Cleaned_Ingredients"].strip("][").split(',')],
+        }
+        return_data.append(data)
+    return return_data
+
+
+def search_recipe(query,type):
+    misspelled = spell_checking(query)
+    if len(misspelled) != 0:
+        return {"did_you_mean": [x for x in misspelled]}
+
+    if type == 'ingredient':
+        return search_by_ingreidents(query)
+
+    if type == 'title':
+        return search_by_title(query)
+
+    if type == 'instruction':
+        return search_by_instruciton(query)
+
+
+def get_recipe_details(id):
+    return {
+            'id': int(id),
+            'title': food.iloc[id].to_dict()["Title"],
+            'instructions': [x.lstrip().strip("'") for x in str(food.iloc[id].to_dict()["Instructions"]).split('.')],
+            'image_Name': food.iloc[id].to_dict()["Image_Name"],
+            'ingredients': [x.lstrip().strip("'") for x in food.iloc[id].to_dict()["Cleaned_Ingredients"].strip("][").split(',')],
+        }
